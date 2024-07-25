9b4d7e9be471e34a33d2e6d5d69b56aaac1a903d
EastonA01 <eastonredwing@gmail.com>
2024-07-25 15:52:07 -0400

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,12 @@
+
+
+public class Animal {
+    private final String name;
+    private final boolean isDog;
+
+    // Constructor
+    public Animal(String dogName, boolean dog) {
+        this.name = dogName;
+        isDog = dog;
+    }
+}
